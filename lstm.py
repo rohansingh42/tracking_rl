6652bc8464a540370c4f0412c26206ecb478a08f
Arjung27 <arjun.g1511@gmail.com>
2019-12-13 08:49:14 -0500

    fixed by making sure no small episode batches get added(removed break + more stuff). might not train well though. maybe try not to subtract time_steps from randint line

--- lstm.py
+++ lstm.py
@@ -14,10 +14,15 @@ from datetime import datetime
 import glob, os 
 import argparse
 from collections import deque
+import pygame
+import random
+from Game import Game
 
-# Load game env
-# env = gameEnv()
-# curr_state = env.reset()
+pygame.init()
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+game = Game()
 
 # Define memory class for experience relay
 class Memory():
@@ -30,6 +35,7 @@ class Memory():
         sampled_episodes = random.sample(self.memory, bsize)
         batch = []
         for episode in sampled_episodes:
+            # print(episode)
             point = np.random.randint(0, len(episode)+1-time_step)
             batch.append(episode[point:point+time_step])
         return batch
@@ -46,9 +52,9 @@ GAMMA = 0.99
 INITIAL_EPSILON = 1.0
 FINAL_EPSILON = 0.1
 TOTAL_EPISODES = 20000
-MAX_STEPS = 50
+MAX_STEPS = 500
 MEMORY_SIZE = 3000 # size of the list to store last n episodes
-UPDATE_FREQ = 5
+UPDATE_FREQ = 10
 PERFORMANCE_SAVE_INTERVAL = 200 
 TARGET_UPDATE_FREQ = 20000 # num of steps
 
@@ -64,8 +70,8 @@ class Network(nn.Module):
         # check how to include batches
         self.fc1 = nn.Linear(self.input_size, 
                              self.hidden_layer1)
-        self.fc2 = nn.Linear(self.fc1, self.fc2)
-        self.fc3 = nn.Linear(self.fc2, self.fc3)
+        self.fc2 = nn.Linear(self.hidden_layer1, self.hidden_layer2)
+        self.fc3 = nn.Linear(self.hidden_layer2, self.hidden_layer3)
         self.lstm_layer = nn.LSTM(input_size = 32, 
                                   hidden_size = 32,
                                   num_layers = 1, 
@@ -74,14 +80,21 @@ class Network(nn.Module):
         self.val = nn.Linear(32, 1)
 
     def forward(self, x, bsize, time_step, hidden_state, cell_state):
-        x = x.view(bsize*time_step, self.input_size)
+        x = x.view(bsize*time_step,1, self.input_size)
         model = torch.nn.Sequential(self.fc1, 
                                     nn.ReLU(inplace=False),
                                     self.fc2,
                                     nn.ReLU(inplace=False),
                                     self.fc3,
                                     nn.ReLU(inplace=False))
-        lstm_out = (model(x), (hidden_state, cell_state))
+        # out = self.fc1(x)
+        # out = F.ReLU(out)
+        # out = self.fc2(out)
+        # out = F.ReLU(out)
+        # out = self.fc3(out)
+        # out = F.ReLU(out)
+        out = model(x).view(bsize, time_step, 32)
+        lstm_out = self.lstm_layer(out, (hidden_state, cell_state))
         out = lstm_out[0][:, time_step-1, :]
         h_n = lstm_out[1][0]
         c_n = lstm_out[1][1]
@@ -89,8 +102,7 @@ class Network(nn.Module):
         adv_out = self.adv(out)
         val_out = self.val(out)
 
-        Qout = val_out.expand(bsize, self.output_size) + 
-               (adv_out - adv_out.mean(dim=1).unsqueeze(dim=1).expand(bsize, self.output_size))
+        Qout = val_out.expand(bsize, self.output_size) + (adv_out - adv_out.mean(dim=1).unsqueeze(dim=1).expand(bsize, self.output_size))
         
         return Qout, (h_n, c_n)
     
@@ -107,21 +119,27 @@ target_model = Network(input_size = INPUT_SIZE, out_size = OUT_SIZE).to(device)
 target_model.load_state_dict(nn_model.state_dict())
 loss_fn = nn.SmoothL1Loss()
 optimizer = torch.optim.Adam(nn_model.parameters(), lr = 0.0001)
+torch.manual_seed(1); np.random.seed(1)
+path = glob.glob(os.path.expanduser('./logs/'))[0]
+SummaryWriter = SummaryWriter('{}{}'.format(path, datetime.now().strftime('%b%d_%H-%M-%S')))
+
 
 # initialize memory
 mem = Memory(memsize = MEMORY_SIZE)
 # Fill the memory for the initial state
 for i in range(0, MEMORY_SIZE):
-    curr_state = env.reset()
+    curr_state = game.reset()
     step_count = 0
     local_memory = []
 
     while step_count < MAX_STEPS:
         step_count += 1
         action = np.random.randint(0,5)
-        next_state, reward, done = env.step(action)
-        local_memory.append(curr_state, action, reward, next_state)
+        next_state, reward, done, success = game.step(action)
+        local_memory.append((curr_state, action, reward, next_state))
         curr_state = next_state
+        # if done or success:
+        #     break
     mem.add_episode(local_memory)
 
 print('Populated with %d Episodes'%(len(mem.memory)))
@@ -132,9 +150,10 @@ epsilon = INITIAL_EPSILON
 loss_stat = []
 reward_stat = []
 total_steps = 0
+n_success = 0
 
-for episode in range(0, TOTAL_EPISODES):
-    curr_state = env.reset()
+for episode in trange(0, TOTAL_EPISODES):
+    curr_state = game.reset()
     episode_reward = 0
     step_count = 0
     episode_loss = 0
@@ -147,7 +166,7 @@ for episode in range(0, TOTAL_EPISODES):
         total_steps += 1
 
         if np.random.rand(1) < epsilon:
-            torch_x = torch.from_numpy(curr_state).float().to(device)
+            torch_x = torch.from_numpy(np.asarray(curr_state)).float().to(device)
             model_out = nn_model.forward(torch_x, bsize = 1, time_step=1,
                                          hidden_state = hidden_state, 
                                          cell_state = cell_state)
@@ -155,7 +174,7 @@ for episode in range(0, TOTAL_EPISODES):
             hidden_state = model_out[1][0]
             cell_state = model_out[1][1]
         else:
-            torch_x = torch.from_numpy(curr_state).float().to(device)
+            torch_x = torch.from_numpy(np.asarray(curr_state)).float().to(device)
             model_out = nn_model.forward(torch_x, bsize=1, time_step=1,
                                          hidden_state = hidden_state,
                                          cell_state = cell_state)
@@ -165,7 +184,7 @@ for episode in range(0, TOTAL_EPISODES):
             cell_state = model_out[1][1]
 
         # update model fro next iteration
-        next_state, reward, done = env.step(action)
+        next_state, reward, done, success = game.step(action)
         episode_reward += reward
 
         local_memory.append((curr_state, action, reward, next_state))
@@ -201,7 +220,7 @@ for episode in range(0, TOTAL_EPISODES):
             rewards = np.array(rewards)
 
             torch_cs = torch.from_numpy(current_states).float().to(device)
-            torch_ac = torch.from_numpy(actions).float().to(device)
+            torch_ac = torch.from_numpy(actions).long().to(device)
             torch_rw = torch.from_numpy(rewards).float().to(device)
             torch_ns = torch.from_numpy(next_states).float().to(device)
 
@@ -212,7 +231,7 @@ for episode in range(0, TOTAL_EPISODES):
 
             Q_s, _ = nn_model.forward(torch_cs, bsize = BATCH_SIZE, time_step = TIME_STEP, hidden_state = hidden_batch, cell_state = cell_batch)
 
-            Q_s_a, _ = Q_s.gather(dim=1, index = torch_ac[:,TIME_STEP-1].unsqueeze(dim=1)).squeeze(dim=1)
+            Q_s_a = Q_s.gather(dim=1, index = torch_ac[:,TIME_STEP-1].unsqueeze(dim=1)).squeeze(dim=1)
 
             loss = loss_fn(Q_s_a, target_values)
 
@@ -231,6 +250,13 @@ for episode in range(0, TOTAL_EPISODES):
             epsilon -= (INITIAL_EPSILON - FINAL_EPSILON)/TOTAL_EPISODES 
         SummaryWriter.add_scalar('data/episode_reward', episode_reward, episode)
         SummaryWriter.add_scalar('data/episode_loss', episode_loss, episode)
+
+        if done or success:
+            if success:
+                n_success += 1
+                SummaryWriter.add_scalar('data/cumulative_success', n_success, episode)
+                SummaryWriter.add_scalar('data/success', 1, episode)
+            # break
 torch.save(nn_model.state_dict(), 'LSTM_CKPT.pth')
 
 
